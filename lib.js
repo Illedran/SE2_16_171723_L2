067683583de01e83ac18f7f8d479f085812d5fe3
Illedran <nrd.nardelli@gmail.com>
2016-11-10 09:54:28 +0100

    Added new item functionality

--- lib.js
+++ lib.js
@@ -1,1 +1,44 @@
-/* */
+/*jslint browser:true */
+
+(function () {
+    "use strict";
+    var showFormBtn = document.getElementById("showformbtn"),
+        form = document.getElementById("form"),
+        submitBtn = document.getElementById("submitbtn"),
+        itemName = document.getElementById("itemname"),
+        itemVal = document.getElementById("itemval"),
+        itemNameErr = document.getElementById("itemname-err"),
+        itemValErr = document.getElementById("itemval-err"),
+        tabBody = document.getElementById("bodyrow"),
+        tabHeader = document.getElementById("headerrow");
+    showFormBtn.addEventListener('click', function () {
+        if (form.style.display === 'block') {
+            form.style.display = 'none';
+        } else {
+            form.style.display = 'block';
+        }
+    }, false);
+    submitBtn.addEventListener('click', function () {
+        var newItemName,
+            newItemVal = parseInt(itemVal.value, 10);
+        if (itemName.value.length > 0) {
+            newItemName = itemName.value;
+            itemNameErr.innerHTML = "";
+        } else {
+            itemNameErr.innerHTML = "Empty!";
+        }
+        if (isNaN(newItemVal)) {
+            itemValErr.innerHTML = "Not a number!";
+        } else {
+            itemValErr.innerHTML = "";
+        }
+        if (newItemName && !isNaN(newItemVal)) {
+            var newCellHeader = tabHeader.insertCell(-1),
+                newCellBody = tabBody.insertCell(-1);
+            newCellHeader.innerHTML = newItemName;
+            newCellBody.innerHTML = newItemVal;
+            itemName.value = "";
+            itemVal.value = "";
+        }
+    }, false);
+}());
